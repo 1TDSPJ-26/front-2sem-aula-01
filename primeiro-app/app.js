50b0bdc724a2aeee8a63b84ac5dd5b7b14b1cd30
chrisverbarbuto <chrisvbarbuto@gmail.com>
2026-08-06 11:45:05 -0300

    feat : Projeto base node, morfado para projeto estruturado com src e traspilador ts.

---
+++ primeiro-app/app.js
@@ -0,0 +1,6 @@
+let nome = "Ana";
+// Crie uma concatenação
+//console.log("Olá, " + nome + "!");
+
+//Crie um tamplate string com texto de exemplo
+console.log(`Olá, ${nome}!`);
---
+++ primeiro-app/src/index.ts
@@ -0,0 +1,4 @@
+"use strict";
+const curse: string = "ADS";
+
+console.log(`Curso: ${curse}`);
