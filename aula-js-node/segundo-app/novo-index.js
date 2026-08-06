31eebc00ef1df75539aae99015813db431ba65c7
dudupizzoli <edupizzolijr@gmail.com>
2026-08-06 14:49:02 -0300

    feat : fazendo a tarefa para a aula de amanhã.

---
+++ aula-js-node/segundo-app/novo-index.js
@@ -0,0 +1,3 @@
+let stand = "Star Platinum";
+let poder = "parar o tempo";
+console.log(`Este é o ${stand} e o poder dele é ${poder}.`);
---
+++ aula-js-node/segundo-app/src/index.ts
@@ -0,0 +1,4 @@
+let nome: string = "Jotaro";
+let sobrenome: string = "Joestar";
+let idade: number = 17;
+console.log(`Este é ${nome} ${sobrenome} e ele tem ${idade} anos de idade.`);
