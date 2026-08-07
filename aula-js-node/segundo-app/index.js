ebe1cd2d8e48878b84abbce1c7cd0def2e9d027a
leite-1309 <leite.lucas1309@gmail.com>
2026-08-06 23:12:20 -0300

    feat: Praticando um pouco de TypeScript visto em aula

---
+++ aula-js-node/segundo-app/index.js
@@ -0,0 +1,12 @@
+const alunos = [{
+        "nome": "Lucas",
+        "idade": 18,
+        "curso": "ADS"
+    },
+    {
+        "nome": "Gui",
+        "idade": 21,
+        "curso": "Design"
+    }];
+
+console.log(alunos)
---
+++ aula-js-node/segundo-app/src/index.ts
@@ -0,0 +1,12 @@
+let alunos = [{
+        "nome": "Lucas",
+        "idade": 18,
+        "curso": "ADS"
+    },
+    {
+        "nome": "Gui",
+        "idade": 21,
+        "curso": "Design"
+    }];
+
+console.log(alunos)
